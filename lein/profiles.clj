e00aba982b67147aad61213d8576e396ae7558de
Minsun Lee <dalzony@gmail.com>
2016-04-16 13:28:33 +0900

    Modify

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,7 +1,7 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]
+{:user {:plugins [;[refactor-nrepl "2.0.0-SNAPSHOT"]
                   [lein-environ "1.0.0"]]
         :dependencies [[acyclic/squiggly-clojure "0.1.4"]
                        [org.clojure/tools.nrepl "0.2.12"]]}
+ :repl {:plugins [[cider/cider-nrepl "0.11.0"]]}
  :env {:squiggly {:checkers [:eastwood :typed :kbit]
                   :eastwood-exclude-linters [:unlimited-use]}}}

fb071a47e7a2b0525bd43bdce14707b5d03f1ea6
Minsun Lee <dalzony@gmail.com>
2016-05-02 18:13:24 +0900

    lein exec 추가

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [;[refactor-nrepl "2.0.0-SNAPSHOT"]
-                  [lein-environ "1.0.0"]]
+                  [lein-environ "1.0.0"]
+                  [lein-exec "0.3.6"]]
         :dependencies [[acyclic/squiggly-clojure "0.1.4"]
                        [org.clojure/tools.nrepl "0.2.12"]]}
  :repl {:plugins [[cider/cider-nrepl "0.11.0"]]}

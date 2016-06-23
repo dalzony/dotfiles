74d103846673d94636bfbc01f9c185c6624554e5
Minsun Lee <dalzony@gmail.com>
2016-06-23 14:12:22 +0900

    spcaemacs설정으로 변경

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,8 +1,6 @@
-{:user {:plugins [;[refactor-nrepl "2.0.0-SNAPSHOT"]
-                  [lein-environ "1.0.0"]
-                  [lein-exec "0.3.6"]]
-        :dependencies [[acyclic/squiggly-clojure "0.1.4"]
-                       [org.clojure/tools.nrepl "0.2.12"]]}
- :repl {:plugins [[cider/cider-nrepl "0.11.0"]]}
- :env {:squiggly {:checkers [:eastwood :typed :kbit]
-                  :eastwood-exclude-linters [:unlimited-use]}}}
+{:repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
+
+

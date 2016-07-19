ae79abec7dee250736e0496c147131891b8601d5
Minsun Lee <dalzony@gmail.com>
2016-07-19 11:10:37 +0900

    line-exec 추가

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.13.0-SNAPSHOT"]
                   [lein-environ "1.0.0"]
-                  [lein-try "0.4.3"]]
+                  [lein-try "0.4.3"]
+                  [lein-exec "0.3.6"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [acyclic/squiggly-clojure "0.1.5"]]}}

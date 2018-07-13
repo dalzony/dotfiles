f644051087beb0c95cdfcf87aa914a57656b12c2
Minsun Lee <dalzony@gmail.com>
2018-07-13 14:17:08 +0900

    delete file

--- lein/profiles.clj
+++
@@ -1,7 +0,0 @@
-{:user {:plugins [[cider/cider-nrepl "0.15.1"]]
-        :plugin-repositories [["mail-release" {:url "http://maven.daumcorp.com/content/repositories/dk-mailcloud-release"
-                                               :update :always}]
-                              ["mail-snapshot" {:url "http://maven.daumcorp.com/content/repositories/dk-mailcloud-snapshots"
-                                                :update :always}]]}}
-
-
